672ce2a0cb73f099e1083189775aea5bcb9dc3fe
Eddie Saunders <saunders.eddie@outlook.com>
2021-11-06 18:23:47 -0400

    Changewd API server port to 3001

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const server = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const userRoutes = require('./routes/routes.js')
 
 server.use(express.urlencoded({ extended: true }));
