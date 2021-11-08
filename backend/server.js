c236fe1763413f368ef802ddbd770ab86ed4abba
Eddie Saunders <saunders.eddie@outlook.com>
2021-11-08 05:59:33 -0500

    reworking folder structure for readability

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
-const session = require ('express-session');
+const session = require('express-session');
 const MongoStore = require("connect-mongo");
 const dbConnection = require("./database/database.js"); // loads our connection to the mongo database
-const userRoutes = require('./routes/routes.js')
+const passport = require('./passport');
+const userRoutes = require('./routes');
 const server = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,20 +13,22 @@ server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 server.use(
     session({
-      secret: process.env.SECRET || "this is the default passphrase",
-      store: MongoStore.create({ mongoUrl: process.env.PROD }),
-      resave: false,
-      saveUninitialized: false,
+        secret: process.env.SECRET || "this is the default passphrase",
+        store: MongoStore.create({ mongoUrl: process.env.PROD }),
+        resave: false,
+        saveUninitialized: false,
     })
 );
 
+server.use(passport.initialize());
+server.use(passport.session());
 
 if (process.env.NODE_ENV === "production") {
     server.use("/static", express.static(path.join(__dirname, "../frontend/build/static")));
     server.get("/", (req, res) => { res.sendFile(path.join(__dirname, "../frontend/build/")); });
 }
 
-server.use (userRoutes);
+server.use(userRoutes);
 
 // Base Error handler
 server.use(function (err, req, res, next) {
