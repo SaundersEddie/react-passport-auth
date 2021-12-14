55dafdbf43c8fd490e4fc4695c0a50379f53d48a
Eddie Saunders <saunders.eddie@outlook.com>
2021-12-14 15:43:15 -0500

    Testing props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,12 @@ import './App.css';
 // import AUTH from "./utils/Auth";
 // import API from "./utils/API";
 
-import Home from './Pages/Home';
-import About from './Components/About';
-import Register from './Pages/Register';
-import AdminOnly from './Pages/Auth/AdminOnly';
-import AuthOnly from './Pages/Auth/AuthOnly';
-import Login from './Pages/Login';
+// import Home from './Pages/Home';
+// import About from './Components/About';
+// import Register from './Pages/Register';
+// import AdminOnly from './Pages/Auth/AdminOnly';
+// import AuthOnly from './Pages/Auth/AuthOnly';
+// import Login from './Pages/Login';
 // import UserInfoContext from "./utils/userInfoContext";
 
 // App needs to verify logged in status, if not logged in then we only display a login page
@@ -24,10 +24,11 @@ export default class App extends Component {
     super();
     this.state = {
       token: '',
-      loggedIn: "Not Logged In",
+      loggedIn: false,
       user: {}
     }
-
+    
+    localStorage.setItem('auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWI2NGU3NzNjZmJiZjdhYzdkNDc0YmIiLCJpYXQiOjE2Mzk0ODA3MjN9.a8z5RrnAh-WtZnC9qRaAG_JUq1pZgdb6ZXi18KzmZ2o')
   }
 
 
@@ -59,66 +60,66 @@ export default class App extends Component {
   // }
 
   componentDidMount() {
-    // this.checkLoginStatus();
-    localStorage.setItem('auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWI2NGU3NzNjZmJiZjdhYzdkNDc0YmIiLCJpYXQiOjE2Mzk0ODA3MjN9.a8z5RrnAh-WtZnC9qRaAG_JUq1pZgdb6ZXi18KzmZ2o')
-    const userToken = localStorage.getItem('auth-token');
-    // console.log (userToken);
-
-    axios
-        .post ('/api/user/test', {
-          headers: {
-            'auth-token': userToken
-          }
-        })
-        .then ( res => {
-          console.log (res);
-      })
+    // axios
+        // .post ('/api/user/test', {
+          // headers: {
+            // 'auth-token': localStorage.getItem('auth-token')
+          // }
+        // })
+        // .then ( res => {
+          // console.log (res);
+      // })
     }
 
-  // handleLogout() {
-  //   this.setState({
-  //     loggedInStatus: "NOT_LOGGED_IN",
-  //     user: {}
-  //   });
-  // }
-
-  // handleLogin(data) {
-  //   this.setState({
-  //     loggedInStatus: "LOGGED_IN",
-  //     user: data.user
-  //   });
-  // }
+  validateStatus() {
+    if (localStorage.getItem('auth-token')) {
+      this.setState ({
+        loggedIn: true
+      })
+    }
+    console.log ('Validate Logged In Status: ', this.state.loggedIn);
+    return this.state.loggedIn;
+  }
 
 // export default function App() {
   render() {
+    
+    // {this.validateStatus()}
     return (
-        <div>
-          <Router>
-            <Container className="p-1" fluid>
-              <Navbar className="border-bottom" bg="transparent" expand="lg" sticky="top">
-                <Navbar.Brand>Authentication and authourization Test</Navbar.Brand>
-                <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-                <Navbar.Collapse id="navbar-toggle">
-                  <Nav>
-                    <Link className='nav-link' to='/'>Home</Link>
-                    <Link className='nav-link' to='/login'>Login</Link>
-                    <Link className='nav-link' to='/admin'>Admin Only</Link>
-                    <Link className='nav-link' to='/auth'>Auth Only</Link>
-                    <Link className='nav-link' to='/register'>Register</Link>
-                  </Nav>
-                  <About />
-                </Navbar.Collapse>
-              </Navbar>
-              <Routes>
-                <Route path="/" exact element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/auth" element={<AuthOnly />} />
-                <Route path="/admin" element={<AdminOnly />} />
-              </Routes>
-            </Container>
-          </Router>
-        </div>
+      <div>
+        <h1>Is Logged In = <validateStatus /></h1>
+      </div>
     )
   }
+  // render() {
+  //   return (
+  //       <div>
+  //         <Router>
+  //           <Container className="p-1" fluid>
+  //             <Navbar className="border-bottom" bg="transparent" expand="lg" sticky="top">
+  //               <Navbar.Brand>Authentication and authourization Test</Navbar.Brand>
+  //               <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+  //               <Navbar.Collapse id="navbar-toggle">
+  //                 <Nav>
+  //                   <Link className='nav-link' to='/'>Home</Link>
+  //                   <Link className='nav-link' to='/login'>Login</Link>
+  //                   <Link className='nav-link' to='/admin'>Admin Only</Link>
+  //                   <Link className='nav-link' to='/auth'>Auth Only</Link>
+  //                   <Link className='nav-link' to='/register'>Register</Link>
+  //                 </Nav>
+  //                 <About />
+  //               </Navbar.Collapse>
+  //             </Navbar>
+  //             <Routes>
+  //               <Route path="/" exact element={<Home />} />
+  //               <Route path="/login" element={<Login />} />
+  //               <Route path="/register" element={<Register />} />
+  //               <Route path="/auth" element={<AuthOnly />} />
+  //               <Route path="/admin" element={<AdminOnly />} />
+  //             </Routes>
+  //           </Container>
+  //         </Router>
+  //       </div>
+  //   )
+  // }
 }
